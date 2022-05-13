3a9bf90492035a63342738658ca6687866e54263
Phoenexus22 <69495186+Phoenexus22@users.noreply.github.com>
2022-05-13 13:09:06 +0930

    Update Compiler.py

--- Compiler.py
+++ Compiler.py
@@ -1,17 +1,47 @@
 import sys
 infile = open(sys.argv[1], "r")
+lines = infile.readlines()
 outfile = open(sys.argv[2], "w")
-outfile.write('#include <stdio.h>')
-outfile.write('#include "typedef.h"')
-outfile.write('int main(int argc, char *argv[]){')
+outfile.write('#include <stdio.h>\n')
+outfile.write('#include "typedef.h"\n')
+outfile.write('int main(int argc, char *argv[]){\n')
 vartype = ""
 memorysize = ""
+largest_index = -1
 def CompilerSettings():
     #prototype order:  source file, future file, memory bitwidth, memory size
-    if sys.argv[3] == ("u8" or "u16" or "u32" or "u64" or "u1" or "i8" or "i16" or "i32" or "i64"):
+    if sys.argv[3] in ["u8", "u16", "u32", "u64", "u1", "i8", "i16", "i32", "i64"]:
         vartype = sys.argv[3]
     else:
         raise Exception("Invalid Datatype")
     memorysize = sys.argv[4]
-    outfile.write(vartype + " _RAM_[" + memorysize + "];")
+    outfile.write(vartype + " _RAM_[" + memorysize + "];\n")
+
+def EndFile():
+    outfile.write("}")
+    outfile.close()
+
+def LineHandle(index):
+    splitparts = lines[index].split(">")
+    if int(splitparts[0]) >largest_index:
+        outfile.write("_" + splitparts[0] + ": ")
+        largest_index = int(splitparts[0])
+    else:
+        raise Exception("Non-Linear line progression at line " + splitparts[0])
+    fsplit = splitparts[1].split(":")
+    #number evaluation before function, leave most of it to c
+    match fsplit[0]:
+        case "set":
+            None#tmp
+        case "out":
+            None#tmp
+
+    
+
+
+    
 CompilerSettings()
+i = 0
+while (i < len(lines)):
+    LineHandle(i)
+EndFile() 

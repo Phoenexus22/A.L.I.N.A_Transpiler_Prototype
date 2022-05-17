395d0fe10f72909c15613d99e8d18f0ff926e9b1
Phoenexus22 <69495186+Phoenexus22@users.noreply.github.com>
2022-05-17 18:02:42 +0930

    Add files via upload

---
+++ Src/newfile.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "typedef.h"
+int main(int argc, char *argv[]){
+i32 _RAM_[2048];
+_0: _RAM_[0]=97;
+_RAM_[1]=98;
+_1: printf("%c", (char) _RAM_[0]);
+printf("%c", (char) _RAM_[1]);
+_RAM_[3]=_RAM_[3]+1;
+if(_RAM_[3]<10){goto *((void*[]){&&_0,&&_1})[1];}
+}
